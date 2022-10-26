244a18b9ee82a760f62533ce0c3cfff33b091596
Kristina <sikiritskaya@gmail.com>
2022-10-26 23:06:14 +0300

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,29 +1,61 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios'
+import got from 'got';
+import request from 'superagent'
 import './App.css';
 import Checkbox from './components/Buttons/Checkbox';
 import Search from './components/Search/Search';
 //import Words from './components/Words/Words';
 
+const URL = 'https://jsonplaceholder.typicode.com/todos?_limit=5'
+
 function App() {
 
-  const [words, setWords] = useState(['california', 'everything', 'aboveboard', 'washington', 'basketball', 'weathering', 'characters', 'literature', 'contraband', 'appreciate'])
-  const [filterList, setFilterList] = useState(words)
-  const [filterLetter, setFilterLetter] = useState(words.slice().sort())
-  
+  const [words, setWords] = useState(null)
+  const [filterList, setFilterList] = useState(null)
+  const [filterLetter, setFilterLetter] = useState(null)
 
+  useEffect(() => {
+    fetch(URL).then(res => res.json())
+      .then(data => {
+        setWords(data)
+        setFilterList(data)
+        setFilterLetter(data)
+      })
+  }, [])
+
+  /* useEffect(() => {
+    axios.get(URL).then(response => {
+      setWords(response.data)
+      setFilterList(response.data)
+      setFilterLetter(response.data)
+    })
+  }, []) */
+
+  /* useEffect(() => {
+    request.get(URL).then(response => {
+      setWords(response.data)
+      setFilterList(response.data)
+      setFilterLetter(response.data)
+    })
+  }, [])
+ */
   const onSearch = (value) => {
     const searchList = filterList.filter(word => {
-      return word.toLocaleLowerCase().includes(value.toLocaleLowerCase())
+      return word.title.toLocaleLowerCase().includes(value.toLocaleLowerCase())
     })
     setWords(searchList)
+
   }
 
   const onChecked = (checked) => {
-    console.log(words)
+    const newArr = filterLetter.slice().sort((a, b) => a.title > b.title ? 1 : -1)
+    console.log('words', words)
+    console.log('filter', filterLetter)
     if (!checked) {
-      return setFilterLetter(filterLetter)
-    } 
-    if(checked){
+      return setFilterLetter(newArr)
+    }
+    if (checked) {
       return setWords(words)
     }
   }
@@ -31,11 +63,12 @@ function App() {
     return true
   }
   return (
-    <>
-      <Search onSearch={onSearch} reset={reset} />
-      <Checkbox words={words} reset={reset} filterLetter={filterLetter} onChecked={onChecked} /* setWords={setWords} */ />
-      {/* <Words words={words} /> */}
-    </>
+    !!words && (
+      <>
+        <Search onSearch={onSearch} reset={reset} />
+        <Checkbox words={words} reset={reset} filterLetter={filterLetter} onChecked={onChecked} /* setWords={setWords} */ />
+        {/* <Words words={words} /> */}
+      </>)
   );
 }
 
--- src/components/Words/Words.jsx
+++ src/components/Words/Words.jsx
@@ -2,10 +2,11 @@ import Word from "./Word";
 
 const Words = ({ words, isChecked, filterLetter }) => {
     const list = !isChecked ? words : filterLetter
+    console.log(words)
   return (
     <div>
-      {list.map((item, index) => (
-        <Word key={index} word={item} />
+      {list.map((item) => (
+        <Word key={item.id} word={item.title} />
       ))}
     </div>
   );
